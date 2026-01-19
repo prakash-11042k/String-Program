488193eb354b5185fe5bbed3d51c4f160e681851
prakash <prakash11042k@gmail.com>
2026-01-19 07:17:19 -0800

    String Program

---
+++ Read_the_Single_word_in_String.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Read_the_Single_word_in_String 
+{
+	static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+		System.out.print("Enter the word:");
+		String word=sc.next();
+		System.out.println("Entered word is:"+word);
+	}
+}
