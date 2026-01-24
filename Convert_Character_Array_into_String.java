6b7133d52fc6a593be5f604303b37a40dc834a5a
prakash <prakash11042k@gmail.com>
2026-01-24 10:45:53 +0530

    String Program

---
+++ Convert_Character_Array_into_String.java
@@ -0,0 +1,11 @@
+class Convert_Character_Array_into_String 
+{
+	public static void main(String[] args) 
+	{
+		char[] a={'A','$','#','b'};
+		String s=new String(a);
+		
+		System.out.println(a);
+		System.out.println(s);
+	}
+}
