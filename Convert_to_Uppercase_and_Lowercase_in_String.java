0b4b325849520c72525577bada7c4bc2ecd8b4e4
prakash <prakash11042k@gmail.com>
2026-01-25 14:03:10 +0530

    String Program

---
+++ Convert_to_Uppercase_and_Lowercase_in_String.java
@@ -0,0 +1,14 @@
+class Convert_to_Uppercase_and_Lowercase_in_String 
+{
+	public static void main(String[] args) 
+	{
+		String x="TABREZ";
+		String y="syed";
+		
+		String res1=x.toLowerCase();
+		String res2=y.toUpperCase();
+		
+		System.out.println(res1);
+		System.out.println(res2);
+	}
+}
