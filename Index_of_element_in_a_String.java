b765252a2558fe03c5a2e1b01c68952f3c4eb533
prakash <prakash11042k@gmail.com>
2026-01-30 10:06:54 +0530

    String Program

---
+++ Index_of_element_in_a_String.java
@@ -0,0 +1,13 @@
+class Index_of_element_in_a_String 
+{
+	public static void main(String[] args) 
+	{
+		String s="ABCDEF";
+		
+		System.out.println(s.indexOf('A'));
+		System.out.println(s.indexOf('D'));
+		System.out.println(s.indexOf('A'));
+		System.out.println(s.indexOf('Z'));
+		System.out.println(s.indexOf('R'));
+	}
+}
