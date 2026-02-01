18e9d28720f6c8c0eba04017f8af0a8e203bfabd
prakash <prakash11042k@gmail.com>
2026-02-01 19:09:13 +0530

    String Program

---
+++ Provide_the_Substring_from_Start_index_till_End_index.java
@@ -0,0 +1,12 @@
+class Provide_the_Substring_from_Start_index_till_End_index 
+{
+	public static void main(String[] args) 
+	{
+		String s="Sachin Tendulkar";
+		
+		System.out.println(s.substring(4));
+		System.out.println(s.substring(10));
+		System.out.println(s.substring(11,15));
+		System.out.println(s.substring(2,8));
+	}
+}
