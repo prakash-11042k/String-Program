1a68c46ae1308b7364db6bd00aba1b8bf866e67c
prakash <prakash11042k@gmail.com>
2026-02-08 14:21:37 +0530

    String Program

---
+++ Convert_String_Integer_into_Integer.java
@@ -0,0 +1,11 @@
+class Convert_String_Integer_into_Integer 
+{
+	public static void main(String[] args) 
+	{
+		String s="10";
+		int res=Integer.parseInt(s);
+		
+		System.out.println(s);
+		System.out.println(res);
+	}
+}
