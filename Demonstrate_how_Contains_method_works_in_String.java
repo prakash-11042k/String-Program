05eca73c11a51f7afa0eb17c2421d17014fa7e6b
prakash <prakash11042k@gmail.com>
2026-02-15 22:16:31 +0530

    String Program

---
+++ Demonstrate_how_Contains_method_works_in_String.java
@@ -0,0 +1,11 @@
+class Demonstrate_how_Contains_method_works_in_String 
+{
+	public static void main(String[] args) 
+	{
+		String s="Tabrez";
+		
+		System.out.println(s.contains("ez"));
+		System.out.println(s.contains("eaz"));
+		System.out.println(s.contains("Tab"));
+	}
+}
